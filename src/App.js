e4a250ddaca7d029cc3a5e5f60282fc1dc520c12
JM Fantin <joao.fantin@acad.pucrs.br>
2020-04-22 11:55:19 -0300

    Frontend ready for the web

---
+++ src/App.js
@@ -0,0 +1,31 @@
+// in src/App.js
+import React from "react";
+import { Admin, Resource } from "react-admin";
+import authProvider from "./authProvider";
+import Dashboard from "./Dashboard";
+import { PostList, PostEdit, PostCreate } from "./posts";
+import { UserList } from "./users";
+import jsonServerProvider from "ra-data-json-server";
+
+import PostIcon from "@material-ui/icons/Book";
+import UserIcon from "@material-ui/icons/Group";
+
+const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
+const App = () => (
+  <Admin
+    dashboard={Dashboard}
+    authProvider={authProvider}
+    dataProvider={dataProvider}
+  >
+    <Resource
+      name="posts"
+      list={PostList}
+      edit={PostEdit}
+      create={PostCreate}
+      icon={PostIcon}
+    />
+    <Resource name="users" list={UserList} icon={UserIcon} />
+  </Admin>
+);
+
+export default App;
